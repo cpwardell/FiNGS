8c8e703d63062cf0dfe3dfbdb477b38cf31d31b0
cpwardell <github@cpwardell.com>
2018-05-01 15:18:11 -0500

    Debugging docker dev
    Former-commit-id: 24fdd433ce58b5566def3435ef26581690e51484

--- install_FiNGS_dependencies.sh
+++ install_FiNGS_dependencies.sh
@@ -3,16 +3,16 @@
 apt-get update
 apt-get -y install python3
 apt-get -y install python3-pip
-pip3 install --upgrade pip
+#pip3 install --upgrade pip
 pip3 install PyVCF
 pip3 install pysam
 pip3 install editdistance
 pip3 install scipy
 pip3 install joblib
 
-apt-get -y install r-base r-base-dev
-apt-get -y install libcurl4-openssl-dev
-apt-get -y install libxml2-dev
-apt-get -y install zlib1g-dev
-Rscript --vanilla R/install_packages.R
+#apt-get -y install r-base r-base-dev
+#apt-get -y install libcurl4-openssl-dev
+#apt-get -y install libxml2-dev
+#apt-get -y install zlib1g-dev
+#Rscript --vanilla R/install_packages.R
 
