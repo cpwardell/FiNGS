87dc63af30a2e9ac0813daab429f882e57566a8c
cpwardell <github@cpwardell.com>
2019-09-10 16:30:31 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fings",
+    version="1.5",
+    author="Christopher Wardell",
+    author_email="github@cpwardell.com",
+    description="Filters for Next Generation Sequencing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cpwardell/FiNGS",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
